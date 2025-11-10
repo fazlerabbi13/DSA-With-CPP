a1e736a2872cfc8ac650abf724fe76987532ccc1
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-10 21:24:49 +0600

    create a class node and declare constructor.

---
+++ BST/fullBST.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
